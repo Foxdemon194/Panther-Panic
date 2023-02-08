05f441e11026e74806becbeb957b0b5c0159040d
alejandro guzman <alejandro.guzman006@mymdc.net>
2023-02-08 13:12:46 -0500

    Very basic start menu made

---
+++ Assets/Scripts/MenuStuff.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuStuff : MonoBehaviour
+{
+    //make a quick menu system, then include animation stuff later
+    public GameObject menu;
+    public GameObject play;
+    public GameObject levels;
+    public GameObject htp;
+    public GameObject credits;
+
+    public void Menu()
+    {
+        SetInactive();
+        menu.SetActive(true);
+    }
+
+    public void Play()
+    {
+        SetInactive();
+        play.SetActive(true);
+    }
+
+    public void Levels()
+    {
+        SetInactive();
+        levels.SetActive(true);
+    }
+
+    public void HowToPlay()
+    {
+        SetInactive();
+        htp.SetActive(true);
+    }
+
+    public void Credits()
+    {
+        SetInactive();
+        credits.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    void SetInactive()
+    {
+        menu.SetActive(false);
+        play.SetActive(false);
+        levels.SetActive(false);
+        htp.SetActive(false);
+        credits.SetActive(false);
+    }
+}
